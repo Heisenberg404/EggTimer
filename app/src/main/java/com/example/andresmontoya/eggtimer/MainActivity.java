cd558a0dc6f47bca2c0d29355aa3a67b0f6af7d7
amontoya49 <andresmhkun@gmail.com>
2016-10-07 19:39:44 -0500

    ajust of images and seekbar

--- app/src/main/java/com/example/andresmontoya/eggtimer/MainActivity.java
+++ app/src/main/java/com/example/andresmontoya/eggtimer/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.andresmontoya.eggtimer;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.CountDownTimer;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,8 +19,10 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
     Button button;
     TextView textView;
+    ImageView imageView;
     CountDownTimer countDownTimer;
     Boolean couter_active = false;
+    Boolean flag_win = false;
 
     public void updateTimer(int seconds_left){
         int minutes = (int) seconds_left/60;
@@ -24,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         String minutes_text = Integer.toString(minutes);
         minutes_text = minutes >= 0 && minutes<10? "0"+Integer.toString(minutes) : Integer.toString(minutes);
         seconds_text = seconds >= 0 && seconds<10? "0"+Integer.toString(seconds) : Integer.toString(seconds);
-        textView.setText(minutes_text+" : "+seconds_text);
+        textView.setText(minutes_text+":"+seconds_text);
+        imageView.setImageResource(R.drawable.egg);
+        flag_win = false;
     }
 
     public void  controlTimer(View view){
@@ -36,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onTick(long millisUntilFinished) {
                     updateTimer((int) millisUntilFinished / 1000);
+                    seekBar.setProgress((int) millisUntilFinished / 1000);
                 }
 
                 @Override
@@ -45,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
                     MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
                     mediaPlayer.start();
                     resetTimer();
+                    flag_win = true;
+                    setImageWin(flag_win);
                 }
             }.start();
         }
@@ -53,15 +65,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = (ImageView) findViewById(R.id.imageView2);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         textView = (TextView) findViewById(R.id.textView2);
         button = (Button) findViewById(R.id.button);
         seekBar.setMax(600);
+        seekBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
+        seekBar.getThumb().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
         seekBar.setProgress(30);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -88,5 +103,12 @@ public class MainActivity extends AppCompatActivity {
         button.setText("Go!");
         seekBar.setEnabled(true);
         couter_active = false;
+    }
+
+    public void setImageWin(Boolean flag_win){
+        if (flag_win)
+            imageView.setImageResource(R.drawable.chicken);
+        else
+            imageView.setImageResource(R.drawable.egg);
     }
 }
